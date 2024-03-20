323e2097aa2d54a3c42475c38a3c391952af1286
YUYA <YuyaSato225>
2024-03-20 20:22:28 +0900

    DTO対応まで、何故か更新されない

---
+++ bot/Dtos/ChatGPTRequest.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class ChatGPTRequest:
+    request: str
+
---
+++ bot/Usecases/ChatGPTClient.py
@@ -0,0 +1,21 @@
+import openai
+from .. import config
+from ..Dtos.ChatGPTRequest import ChatGPTRequest
+from ..Dtos.ChatGPTResponse import ChatGPTResponse
+
+class ChatGPTClient:
+    def send_request(self, request: ChatGPTRequest) -> ChatGPTResponse:
+        openai.api_key = config.OPENAI_API_KEY
+        print("test")
+        response = openai.chat.completions.create(
+            model="gpt-4-turbo-preview",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": request.request},
+            ],
+        )
+        print(response.choices[0].message.content)
+        response = ChatGPTResponse(response=response.choices[0].message.content)
+        return response
+
+
